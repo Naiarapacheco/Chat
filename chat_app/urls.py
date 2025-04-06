0962fe6e601f063b83d97e5a8713a72f52e5c19d
Naiara Pacheco <naiarapachecosantos@gmail.com>
2025-04-05 23:23:20 -0300

    Add routes for login, logout, and signup

--- chat_app/urls.py
+++ chat_app/urls.py
@@ -1,7 +1,11 @@
+from django.contrib.auth import views as auth_views
 from django.urls import path
 
-from . import views
+from .views import CustomLoginView, frontpage, signup
 
 urlpatterns = [
-    path ('', views.frontpage, name='frontpage'),
+    path ('', frontpage, name='frontpage'),
+    path('signup/', signup, name='signup'),
+    path('login/', CustomLoginView.as_view(template_name='main/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout')
 ]
