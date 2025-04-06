53ca9497b7b1436e9f7a0e22f7f0e66757acee73
Naiara Pacheco <naiarapachecosantos@gmail.com>
2025-04-05 23:19:28 -0300

    add user signup logic and custom login error message

--- chat_app/views.py
+++ chat_app/views.py
@@ -1,4 +1,32 @@
-from django.shortcuts import render
+from django.contrib.auth import login
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.views import LoginView
+
+from .forms import SignUpForm
 
 def frontpage(request):
     return render(request, 'main/home.html')
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Usuário cadastrado com sucesso!')
+            return redirect('frontpage')
+    else:
+        form = SignUpForm()
+
+    return render(request, 'main/signup.html', {'form': form})
+
+class CustomLoginView(LoginView):
+    def form_invalid(self, form):
+        username = self.request.POST.get("username", "").strip()
+        password = self.request.POST.get("password", "").strip()
+
+        if username and password:
+            messages.error(self.request, "Usuário ou senha incorretos.")
+            
+        return super().form_invalid(form)
