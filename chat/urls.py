24a3a58567f29e6f7373834b9a2d7a17bcdfec6a
Naiara Pacheco <naiarapachecosantos@gmail.com>
2025-04-08 19:09:36 -0300

    Configure URL routing to include chat views from 'realtime' app

--- chat/urls.py
+++ chat/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include('chat_app.urls')),
+    path('chats/', include('realtime.urls')),
     path('admin/', admin.site.urls),
 ]
